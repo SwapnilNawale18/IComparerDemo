04096c1a36d758c40a0bad87c523fe2be8cebafd
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-31 21:50:13 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IComparerDemo
+{
+    internal class Employee : IComparer<Employee>
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public int Compare(Employee x, Employee y)
+        {
+            return y.Salary - x.Salary;
+        }
+        public override string ToString()
+        {
+            //return base.ToString();
+            return this.Name + "\t" + this.Salary.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IComparerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, empSalary;
+            string empName;
+            List<Employee> lst = new List<Employee>();
+            Console.WriteLine("Enter number of employees");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Enter name of employee {0}: ", i + 1);
+                empName = Console.ReadLine();
+                Console.Write("Enter Salary of employee {0}: ", i + 1);
+                empSalary = Convert.ToInt32(Console.ReadLine());
+                lst.Add(new Employee { Name = empName, Salary = empSalary });
+            }
+            lst.Sort(new Employee());
+            foreach (var i in lst)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
